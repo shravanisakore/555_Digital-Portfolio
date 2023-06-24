26f6297a6f76c7932ed46f1d2ff8363e6d6d236b
shravanisakore <137531557+shravanisakore@users.noreply.github.com>
2023-06-24 10:32:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial scale=3.0">
+    <title>MY PORTFOLIO</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            SHRAVANI SAKORE
+            <nav>
+                <ul>
+                    <li><a href="/Intro.html"></a>My Intro</li>
+                    <li><a href="/Home.html"></a>Home</li>
+                    <li><a href="/Education.html"></a>Education</li>
+                    <li><a href="/Hobbies.html"></a>Hobbies</li>
+                </ul>
+            </nav>
+        </div>
+        <div class="main">
+            <div class="infoContainer">
+                <div class="devInfo">
+                    <div class="hello">Hi I am </div>
+                    <div class="NAME">SHRAVANI SAKORE</div>
+                    <div class="ABOUT">FYBTECH enggineering-Alandi</div>
+                    <div class="buttons">
+                        <button>Download CV</button>
+                        <button>Contact Me</button>
+                    </div>
+                    <div class="devpic"><img src="dev.png"alt="SHRAVANI SAKORE"></div>
+                </div>
+            </div>
+            
+            
+            
+        </div>
+</div>
+</body>
+</html>
